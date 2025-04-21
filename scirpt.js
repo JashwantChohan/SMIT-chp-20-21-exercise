a82381bc6b3f001cc7657086f500048b119794bd
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-04-21 14:38:00 +0500

    9 done

--- scirpt.js
+++ scirpt.js
@@ -36,3 +36,52 @@
 // let indexNo = value.lastIndexOf('l')
 // document.write(`String ${value}<br>`)
 // document.write(`Index of n: ${indexNo}`)
+
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+// let value = "Pakistani"
+// let indexNo = value.charAt(3)
+// document.write(`String ${value}<br>`)
+// document.write(`Character at 3: ${indexNo}`)
+
+// 6. Repeat Q1 using string concat() method.   
+
+// let firstName = prompt("Enter your first Name")
+// let secondName = prompt("Enter your Second Name")
+
+// let fullName = firstName.concat(secondName)
+
+// document.write(`Hello ${fullName}, Welcome to the show`)
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+
+// let word = "Hyderabad"
+// let  result = word.replace("Hyder"," Islam")
+
+// document.write(`City: ${word}<br>`)
+// document.write(`After Replacement: ${result}`)
+
+// 8. Write a program to replace all occurrences of “and” in the
+// string with “&” and display the result in your browser.
+// var message = “Ali and Sami are best friends. They play cricket and
+// football together.”;
+
+// var line = "Ali and Sami are best friends. They play cricket and football together."
+// let result = line.replace(/and/g , "&")
+// document.write(result)
+
+// Write a program that converts a string “472” to a number
+// 472. Display the values & types in your browser.
+
+// let str = "472"
+// let convertedStr = parseInt(str)
+// document.write(`value: ${str}<br>`)
+// document.write(`Type: ${typeof(str)}<br>`)
+// document.write(`value: ${str}<br>`)
+// document.write(`Type: ${typeof(convertedStr)}`)
+
+
