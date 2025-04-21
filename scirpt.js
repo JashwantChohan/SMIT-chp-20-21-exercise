0ec12440bda9ee95e9143689d4fed023f7c1115d
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-04-21 13:43:09 +0500

    second done

--- scirpt.js
+++ scirpt.js
@@ -1,3 +1,22 @@
 // 1. Write a program that takes two user inputs for first and
 // last name using prompt and merge them in a new variable
 // titled fullName. Greet the user using his full name.
+
+// let firstName = prompt("Enter your first Name")
+// let secondName = prompt("Enter your Second Name")
+
+// let fullName = firstName + secondName
+
+// document.write(`Hello ${fullName}, Welcome to the show`)
+
+// 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+
+// let mobileInput = prompt("Enter your favourite mobile phone model")
+
+// let length = mobileInput.length
+
+// document.write(`Your favourite mobile phone is: ${mobileInput}<br>`)
+// document.write(`length of a string is ${length}`)
